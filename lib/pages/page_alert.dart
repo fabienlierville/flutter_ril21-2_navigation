6dfb66a16971a31acd60e3b2b6565ae98c34ff0a
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-19 09:46:16 +0100

    Alert

---
+++ lib/pages/page_alert.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class PageAlert extends StatefulWidget {
+  const PageAlert({Key? key}) : super(key: key);
+
+  @override
+  _PageAlertState createState() => _PageAlertState();
+}
+
+class _PageAlertState extends State<PageAlert> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Alert"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Appuyez moi"),
+          onPressed: (){
+            //Todo
+          },
+        ),
+      ),
+    );
+  }
+}
