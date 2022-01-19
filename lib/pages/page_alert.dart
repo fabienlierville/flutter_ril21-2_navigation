2a118c89ab6184c67551757448b687c55bae07ea
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-19 09:57:03 +0100

    Alert

--- lib/pages/page_alert.dart
+++ lib/pages/page_alert.dart
@@ -30,7 +30,20 @@ class _PageAlertState extends State<PageAlert> {
     showDialog(
       context: context,
       builder: (BuildContext contextDialog){
-        return Text("Coucou");
+        return AlertDialog(
+          title: Text("Ceci est une alerte", textScaleFactor: 2,),
+          content: Text("Nous avons un prolbème avec l'API ...."),
+          contentPadding: EdgeInsets.all(5.0),
+          actions: [
+            TextButton(
+                onPressed: (){
+                  print("Annuler");
+
+                },
+                child: Text("Annuler", style: TextStyle(color: Colors.red),)
+            ),
+          ],
+        );
       }
     );
   }
